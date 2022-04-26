47b926b3c4b32efbf0a40ec65a83b305e8b62a78
Gustavo-Espindola-Olegario <gustavo_espindola1999@hotmail.com>
2022-04-25 22:16:45 -0300

    finalização do projeto

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuSecreto = menu.querySelector(".menu-secreto");
 
 menuSecreto.addEventListener("click", () => {
+    document.body.style.overflow = mostrar ? "hidden" :"initial";
     menu.classList.toggle("on", mostrar)
     mostrar = !mostrar;
 })
